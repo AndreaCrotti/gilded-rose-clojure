1654d1e2d2aa505dca79f6f6e706355617f17976
Andrea Crotti <andrea.crotti.0@gmail.com>
2017-02-20 20:29:55 +0000

    more stuff

--- src/gilded_rose/core.clj
+++ src/gilded_rose/core.clj
@@ -5,27 +5,29 @@
 (def ^:const ITEMS-CONFIG
   {:brie {:name "Aged Brie" :sell-in 2 :quality 0}
    :elixir {:name "Elixir of the Mongoose" :sell-in 5 :quality 7}
-   :vest {:name "Elixir of the Mongoose" :sell-in 10 :quality 20}
-   :sulfuras {:name "Elixir of the Mongoose" :sell-in 0 :quality 80}
-   :backstage {:name "Elixir of the Mongoose" :sell-in 15 :quality 20}})
+   :vest {:name "+5 Dexterity Vest" :sell-in 10 :quality 20}
+   :sulfuras {:name "Sulfuras, Hand Of Ragnaros" :sell-in 0 :quality 80}
+   :backstage {:name "Backstage passes to a TAFKAL80ETC concert" :sell-in 15 :quality 20}})
 
+
+(def ITEMS-NAMES (zipmap (keys ITEMS-CONFIG) (map :name (vals  ITEMS-CONFIG))))
 (def INITIAL-INVENTORY (vals ITEMS-CONFIG))
 
 (defn update-quality [items]
   (map
    (fn [item] (cond
-               (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
+               (and (< (:sell-in item) 0) (= (-> :backstage ITEMS-NAMES) (:name item)))
                (merge item {:quality 0})
-               (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
-               (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
+               (or (= (:name item) (-> ITEMS-NAMES :brie)) (= (:name item) (-> :backstage ITEMS-NAMES)))
+               (if (and (= (:name item) (-> :backstage ITEMS-NAMES)) (>= (:sell-in item) 5) (< (:sell-in item) 10))
                  (merge item {:quality (inc (inc (:quality item)))})
-                 (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
+                 (if (and (= (:name item) (-> :backstage ITEMS-NAMES)) (>= (:sell-in item) 0) (< (:sell-in item) 5))
                    (merge item {:quality (inc (inc (inc (:quality item))))})
                    (if (< (:quality item) 50)
                      (merge item {:quality (inc (:quality item))})
                      item)))
                (< (:sell-in item) 0)
-               (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
+               (if (= (-> :backstage ITEMS-NAMES) (:name item))
                  (merge item {:quality 0})
                  (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
                    (merge item {:quality (- (:quality item) 2)})
--- tests/core_test.clj
+++ tests/core_test.clj
@@ -7,17 +7,19 @@
 
 (t/deftest test-requirements
   (t/testing "quality decreasing with time"
-    (let [mongoose (core/item MONGOOSE 5 7)]
+    (let [mongoose (:elixir core/ITEMS-CONFIG)]
       (t/is (= (core/update-quality [mongoose])
                (list {:name "Elixir of the Mongoose", :sell-in 4, :quality 6})))))
 
   (t/testing "quality never below 0"
-    (let [mongoose (core/item MONGOOSE 2 0)
+    (let [mongoose (:elixir core/ITEMS-CONFIG)
           composed (apply comp (repeat 10 core/update-quality))]
-      #_(t/is (= (composed [mongoose]) []))))
+      (t/is (= (:quality (first (composed [mongoose])) 0)))))
 
   (t/testing "Quality never above 50"
     ;; this actually does work
-    (let [mongoose (core/item "Aged Brie" 5 7)
+    (let [mongoose (:brie core/ITEMS-CONFIG)
           composed (apply comp (repeat 100 core/update-quality))]
-      (t/is (= (:quality (first (composed [mongoose]))) 50)))))
+      (t/is (= (:quality (first (composed [mongoose]))) 50))))
+
+  #_(t/testing "backstage"))
